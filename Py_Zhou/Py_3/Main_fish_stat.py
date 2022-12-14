fe08458cf155c572a48b0583b1b52056d17b02a7
Zhou7676 <zzhou7676@gmail.com>
2022-12-14 17:25:02 +0000

    Test to save data in local drive in python

--- Py_Zhou/Py_3/Main_fish_stat.py
+++ Py_Zhou/Py_3/Main_fish_stat.py
@@ -1,23 +1,62 @@
 from BoxClass1 import FishBox
+import os
+
+
+def save_file(file_name, L_newRecord):
+    flag = False
+    if type(L_newRecord) != list:
+        print("The type of context must be list!")
+        return flag
+
+    cur_path = os.path.abspath(os.path.curdir)
+    cur_path = cur_path + '\\' + file_name
+
+    try:
+        f1 = open(cur_path, 'w')
+        f1.writelines(L_newRecord)
+        flag = True
+    except:
+        flag = False
+    finally:
+        if flag:
+            f1.close()
+
+    return flag
+
+
+L_SaveData = []
 
 big_gift_box = FishBox(60, 30, 40)
 big_gift_box.price = 1000
 big_gift_box.amount = 20
 big_gift_box.weight = 10
-print("The volume of big gift box is %d" % big_gift_box.volume())
-print("The area of big gift box is %d" % big_gift_box.area())
+L_SaveData.append("The volume of big gift box is " + str(big_gift_box.volume()))
+L_SaveData.append("The area of big gift box is" + str(big_gift_box.area()))
+# print("The volume of big gift box is %d" % big_gift_box.volume())
+# print("The area of big gift box is %d" % big_gift_box.area())
 index = big_gift_box.type.index('large box')
 g_box_num = big_gift_box.CountBoxNums(200, index)
-print("200 fishes need %d big gift box" % g_box_num)
-print("The total value of big gift box is %d" % (g_box_num * big_gift_box.price))
+L_SaveData.append("200 fishes need " + str(g_box_num) + "big gift box")
+L_SaveData.append("The total value of big gift box is " + str(g_box_num * big_gift_box.price))
+# print("200 fishes need %d big gift box" % g_box_num)
+# print("The total value of big gift box is %d" % (g_box_num * big_gift_box.price))
 
 small_gift_box = FishBox(50, 20, 30)
 small_gift_box.price = 500
 small_gift_box.amount = 10
 small_gift_box.weight = 5
-print("The volume of small gift box is %d" % small_gift_box.volume())
-print("The area of small gift box is %d" % small_gift_box.area())
+L_SaveData.append("The volume of small gift box is " + str(small_gift_box.volume()))
+L_SaveData.append("The area of small gift box is " + str(small_gift_box.area()))
+# print("The volume of small gift box is %d" % small_gift_box.volume())
+# print("The area of small gift box is %d" % small_gift_box.area())
 index = small_gift_box.type.index('small box')
 g_box_num = small_gift_box.CountBoxNums(200, index)
-print("200 fishes need %d small gift box" % g_box_num)
-print("The total value of small gift box is %d" %(g_box_num * small_gift_box.price))
+L_SaveData.append("200 fishes need " + str(g_box_num) + "small gift box")
+L_SaveData.append("The total value of small gift box is " + str(g_box_num * small_gift_box.price))
+# print("200 fishes need %d small gift box" % g_box_num)
+# print("The total value of small gift box is %d" % (g_box_num * small_gift_box.price))
+
+if save_file('fish_records.txt', L_SaveData):
+    print("The data has been saved successfully!")
+else:
+    print("The operation of data saving fails!!")
---
+++ Py_Zhou/Py_4/Build_new_file.py
@@ -0,0 +1,13 @@
+import os
+
+os.path.abspath(os.path.curdir)
+
+# newfile = r'D:\zzhou\\STM32\STM32F429IGTx_WildFire\Py_Zhou\Py_4\t2.txt'
+# b_new_file = open(newfile, 'r')
+# t_n = b_new_file.write("This is a test for python")
+# r_n = b_new_file.read()
+
+# print("%s has been created successfully" %(newfile))
+# print(r_n)
+# b_new_file.close()
+
