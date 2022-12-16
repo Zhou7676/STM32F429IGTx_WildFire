bc4ae95f5720884b402f9e4f53af3efb7c9162cc
Zhou7676 <zzhou7676@gmail.com>
2022-12-16 15:57:41 +0000

    Test SAX to parse XML

---
+++ Py_Zhou/Py_7/SAX_parse_XML.py
@@ -0,0 +1,38 @@
+import xml.sax
+
+
+class GetStorehouse(xml.sax.ContentHandler):
+    def __init__(self):
+        self.CurrentData = ""
+        self.title = ""
+        self.name = ""
+        self.amount = ""
+        self.price = ""
+
+    def startElement(self, label, attributes):
+        self.CurrentData = label
+        if label == "goods":
+            category = attributes["category"]
+            return category
+
+    def endElement(self, label):
+        global get_record
+
+        if self.CurrentData == "title":
+            get_record.append(self.title)
+        elif self.CurrentData == "name":
+            get_record.append(self.name)
+        elif self.CurrentData == "amount":
+            get_record.append(self.amount)
+        elif self.CurrentData == "price":
+            get_record.append(self.price)
+
+    def characters(self, content):
+        if self.CurrentData == "title":
+            self.title = content
+        elif self.CurrentData == "name":
+            self.name = content
+        elif self.CurrentData == "amount":
+            self.amount = content
+        elif self.CurrentData == "price":
+            self.price = content
